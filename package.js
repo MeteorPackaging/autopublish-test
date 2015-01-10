2d23eddcdff8331b77465606a67c1d49adc6d5a8
Luca Mussi <ing.luca.mussi@gmail.com>
2015-01-10 15:25:33 +0100

    Bump to version 0.0.14

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'packaging:autopublish-test',
   summary: 'Dummy package to test auto-publish with TravisCI. DO NOT USE!',
-  version: '0.0.13',
+  version: '0.0.14',
   git: 'https://github.com/MeteorPackaging/autopublish-test.git'
 });
 
