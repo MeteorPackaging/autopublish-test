df766b66b081a0432c8a664d9ce910bb5b3e4a69
Luca Mussi <ing.luca.mussi@gmail.com>
2015-01-11 18:13:34 +0100

    back to correct name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'packaging:autopublish-test2',
+  name: 'packaging:autopublish-test',
   summary: 'Dummy package to test auto-publish with TravisCI. DO NOT USE!',
   version: '0.0.21',
   git: 'https://github.com/MeteorPackaging/autopublish-test.git'
